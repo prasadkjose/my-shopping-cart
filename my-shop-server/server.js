d36e3c126e306e8b2dcdb15db9bc839dda13c2fe
Prasad Koshy Jose <prasadkjose@gmail.com>
2020-08-06 17:34:02 +0200

    Updated env variables for deployment

--- my-shop-server/server.js
+++ my-shop-server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 
 const db = require("./app/models");
@@ -17,11 +18,12 @@ db.mongoose
     process.exit();
   });
 
-var corsOptions = {
-  origin: "http://localhost:3000",
-};
+//Blacklist and whitelist IP addresses
+// var corsOptions = {
+//   origin: "0.0.0.0:",
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
@@ -36,7 +38,7 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 require("./app/routes/routes")(app);
-
+console.log("posrt");
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
